5006dc74ef82b3e0274bf5b7d7373254a2cc61e8
Nitin kumar <104756638+Nitinkumargits@users.noreply.github.com>
2023-09-24 15:02:29 +0530

    add eventListener to login btn

--- script.js
+++ script.js
@@ -80,38 +80,33 @@ const displayMovements = function (movements) {
   });
 };
 
-displayMovements(account1.movements);
 // ///////////////////////////////////////////////////
-const calcDisplayBalance = function (movements) {
-  const balance = movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${balance}€`;
+const calcDisplayBalance = function (acc) {
+  acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${acc.balance}€`;
 };
-calcDisplayBalance(account1.movements);
 // ///////////////////////////////////////////////////
-const calcDisplaySummary = function (movements) {
-  const income = movements
+const calcDisplaySummary = function (acc) {
+  const incomes = acc.movements
     .filter(mov => mov > 0)
-    .reduce((acc, cur) => acc + cur, 0);
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes}€`;
 
-  labelSumIn.textContent = `${income}€`;
-
-  const outcome = movements
+  const out = acc.movements
     .filter(mov => mov < 0)
-    .reduce((acc, cur) => acc + cur, 0);
-  labelSumOut.textContent = `${Math.abs(outcome)}€`;
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(out)}€`;
 
-  const interest = movements
+  const interest = acc.movements
     .filter(mov => mov > 0)
-    .map(deposite => (deposite * 1.2) / 100)
-    .filter((intt, i, Arr) => {
-      console.log(Arr);
-      return intt >= 1;
+    .map(deposit => (deposit * acc.interestRate) / 100)
+    .filter((int, i, arr) => {
+      // console.log(arr);
+      return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-
   labelSumInterest.textContent = `${interest}€`;
 };
-calcDisplaySummary(account1.movements);
 // ///////////////////////////////////////////////////
 const createUserName = function (accs) {
   accs.forEach(function (acc) {
@@ -125,8 +120,55 @@ const createUserName = function (accs) {
 createUserName(accounts);
 // console.log(accounts);
 
+// EVENT HANDLER
+
+const updateUI = function (acc) {
+  // Display movements
+  displayMovements(acc.movements);
+
+  // Display balance
+  calcDisplayBalance(acc);
+
+  // Display summary
+  calcDisplaySummary(acc);
+};
+
+let currentAccount;
+
+btnLogin.addEventListener('click', function (e) {
+  // Prevent form from submitting
+  e.preventDefault();
+
+  currentAccount = accounts.find(
+    acc => acc.username === inputLoginUsername.value
+  );
+  // console.log(currentAccount);
+
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    // Display UI and message
+    labelWelcome.textContent = `Welcome back, ${
+      currentAccount.owner.split(' ')[0]
+    }`;
+    containerApp.style.opacity = 100;
+
+    // Clear input fields
+    inputLoginUsername.value = inputLoginPin.value = '';
+    inputLoginPin.blur();
+
+    // Update UI
+    updateUI(currentAccount);
+  }
+});
+
 ///////////////////////////////////////////////
 ///////////////////////////////////////////////
+
+// const account = accounts.find(acc => acc.owner === 'Jessica Davis');
+// console.log(account);
+
+// for (const acc of accounts) {
+//   acc.owner == 'Jessica Davis' ? console.log(acc) : null;
+// }
 
 // fliter method
 
